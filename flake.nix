1a2f9693f6f62a164a25db8ac9684f3a43d6c813
Daniel Tabellion <wellos94@web.de>
2022-10-11 20:44:40 +0200

    move to nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = with pkgs; pkgs.mkShell {
+
+          buildInputs = [
+            cargo
+            # wasm-pack
+            rustc
+            rustfmt
+            rustPackages.clippy
+            # cargo-web
+            alsa-lib
+            pkg-config
+            libpulseaudio
+            cmake
+          ];
+        };
+      });
+
+}
+
